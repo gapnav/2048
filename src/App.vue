acba492aa15efc9b4d13eb11fa924281e59e910c
GAP <gapnav@gmail.com>
2018-10-20 00:24:07 +0300

    install and configure Vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  #app
+    | Hello
+    .sp span
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="scss">
+  #app {
+    background: #eee;
+    color: blue;
+    .sp {
+      color: $col;
+    }
+  }
+</style>
